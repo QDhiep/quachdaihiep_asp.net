849a4e4cab6692b1404618f46249f706be8e8c1a
quachdaihiep <hiepga1752004@gmail.com>
2025-03-28 16:01:52 +0700

    first commit

---
+++ quachdaihiep_th2/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using quachdaihiep_th2.Models;
+
+namespace quachdaihiep_th2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; } // Định nghĩa bảng trong DB
+    }
+}
---
+++ quachdaihiep_th2/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace quachdaihiep_th2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Price { get; set; }
+    }
+}
